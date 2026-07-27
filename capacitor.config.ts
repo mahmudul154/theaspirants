0cd46c99d0baf6da79c1a520d0e49008e9140131
mahmudul154 <aakashh060@gmail.com>
2026-07-27 15:37:19 -0500

    Your descriptive commit message here

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aspirants.app',
+  appName: 'aspirants',
+  webDir: 'dist'
+};
+
+export default config;
