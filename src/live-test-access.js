4a1162e8dfb9befbd6a20a075b8cc55a7b4b2fe3
Mahmudul Hasan <mahmudul154@users.noreply.github.com>
2026-09-10 18:21:30 +0000

    Allow Nafis to use live exam test mode

--- src/live-test-access.js
+++ src/live-test-access.js
@@ -3,7 +3,7 @@
 // leaderboard result.
 export const LIVE_TEST_ALLOWED_EXAM_ID = 'bcs-40-day-model-2026-09-08'
 export const LIVE_TEST_ADDITIONAL_EXAM_IDS = ['bcs-40-day-model-2026-09-10']
-export const LIVE_TEST_ADMIN_EMAILS = ['aakashh060@gmail.com']
+export const LIVE_TEST_ADMIN_EMAILS = ['aakashh060@gmail.com', 'nafischy451@gmail.com']
 
 const allowedExamIds = new Set([LIVE_TEST_ALLOWED_EXAM_ID, ...LIVE_TEST_ADDITIONAL_EXAM_IDS])
 
